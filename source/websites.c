e013ea653532b72b36b33e51bfcbb8abcf592a54
Joseph Kligel <jkligel@gmail.com>
2020-03-12 06:39:48 -0500

    Add new C programs, fleshing out my progress in the C language

---
+++ source/websites.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	int x;
+	puts("What website would you like to go to?");
+	puts("1) LinkeIn Learning");
+	puts("2) Udemy");
+	puts("3) Quora");
+	puts("4) Reddit");
+	printf("Type number here: ");
+	scanf("%d", &x);
+	switch (x) {
+		case 1: system("google-chrome 'https://www.linkedin.com/learning/me/in-progress'");
+				break;
+		case 2: system("google-chrome 'https://www.udemy.com/home/my-courses/learning/'");
+				break;
+		case 3: system("google-chrome 'https://quora.com'");
+				break;
+		case 4: system("google-chrome 'https://reddit.com'");
+				break;
+		default: puts("Invalid Input");
+	}
+
+	return (0);
+}
